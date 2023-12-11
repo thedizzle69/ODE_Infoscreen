ff9bcbc4f0ff291eefd73df51855a491148e7b6d
thedizzle69 <jai.singh29192@gmail.com>
2023-12-11 02:26:01 +0100

    Created basic client and server. fxml issues

--- src/main/java/server/ServerApp.java
+++ src/main/java/server/ServerApp.java
@@ -4,6 +4,7 @@ import client.Content;
 // import resources.ContentType;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,43 +17,72 @@ import java.net.Socket;
 import java.util.Objects;
 
 public class ServerApp extends Application {
+    private ServerSocket serverSocket;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ServerApp.fxml")));
-        primaryStage.setTitle("InfoScreen Server");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ServerApp.fxml")));
+            primaryStage.setTitle("InfoScreen Server");
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.show();
 
-        startServer();
+            startServer();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle FXML loading exception here
+        }
+    }
+
+    @Override
+    public void stop() {
+        // Perform cleanup and close resources (e.g., ServerSocket)
+        // This method is called when the application is stopped.
+        // Make sure to stop the server gracefully.
+        // For simplicity, you can just close the ServerSocket in this example.
+        try {
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void startServer() {
         try (ServerSocket serverSocket = new ServerSocket(5555)) {
             System.out.println("Server is running. Waiting for client connection...");
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected.");
+            while (!Thread.interrupted()) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client connected.");
 
-                new Thread(() -> processClientContent(clientSocket)).start();
+                    new Thread(() -> processClientContent(clientSocket)).start();
+                } catch (IOException e) {
+                    e.printStackTrace(); // Handle the exception or log it
+                }
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Handle the exception or log it
         }
     }
 
+
     private void processClientContent(Socket clientSocket) {
-        try (ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream())) {
+        try {
+            ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
             Content receivedContent = (Content) inputStream.readObject();
             System.out.println("Received content from client: " + receivedContent.getData());
 
-            String processedContent = ContentProcessor.processContent(receivedContent);
-            ScreenOutput.displayContent(processedContent);
+            // Perform UI update on the JavaFX Application Thread
+            Platform.runLater(() -> ScreenOutput.displayContent(ContentProcessor.processContent(receivedContent)));
 
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
