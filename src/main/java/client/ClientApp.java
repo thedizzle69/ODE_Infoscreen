eb79116b47a2221c42eeda3b17f68575bfd2bffc
thedizzle69 <jai.singh29192@gmail.com>
2023-12-11 03:04:04 +0100

    fxml path issues

--- src/main/java/client/ClientApp.java
+++ src/main/java/client/ClientApp.java
@@ -6,18 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class ClientApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
-        Parent root = loader.load();
+        String fxmlPath = "GUI.fxml";
+        URL resourceUrl = getClass().getResource(fxmlPath);
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Client App");
-        primaryStage.show();
-
+        if (resourceUrl == null) {
+            System.err.println("FXML file not found: " + fxmlPath);
+        } else {
+            FXMLLoader loader = new FXMLLoader(resourceUrl);
+            try {
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                primaryStage.setScene(scene);
+                primaryStage.setTitle("Client App");
+                primaryStage.show();
+            } catch (IOException e) {
+                System.err.println("Error loading FXML file: " + fxmlPath);
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
