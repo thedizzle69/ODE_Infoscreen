d2da5e0e76e897426bb980790d2782e5b8729e9c
Jai Singh <134519518+thedizzle69@users.noreply.github.com>
2023-12-11 03:31:17 +0100

    Update ClientApp.java

--- src/main/java/client/ClientApp.java
+++ src/main/java/client/ClientApp.java
@@ -6,18 +6,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class ClientApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
-        Parent root = loader.load();
+    public void start(Stage primaryStage) {
+        String fxmlPath = "GUI.fxml";
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Client App");
-        primaryStage.show();
+        // Resolve the path to the FXML file
+        Path path = FileSystems.getDefault().getPath(fxmlPath);
 
+        try {
+            // Load FXML file directly from the file system
+            Parent root = FXMLLoader.load(path.toUri().toURL());
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Client App");
+            primaryStage.show();
+        } catch (IOException e) {
+            System.err.println("Error loading FXML file: " + fxmlPath);
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
