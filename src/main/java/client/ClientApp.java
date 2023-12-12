a8c38bd85b3c69417cf4b747beb11008d094e9a7
Jai Singh <134519518+thedizzle69@users.noreply.github.com>
2023-12-12 03:25:02 +0100

    lots of stuff done, but cant get the main app loaded after the login screen (#30)
    * deleted duplicate fxml files
    
    * Added login screen but it loops with correct credentials instead of opening the main app. With wrong credentials it correctly prints the error message
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why

--- src/main/java/client/ClientApp.java
+++ src/main/java/client/ClientApp.java
@@ -1,6 +1,7 @@
 package client;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,6 +15,76 @@ public class ClientApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        System.out.println("Hello There...");
+
+        LoginController loginController = openLoginScreen(primaryStage);
+
+        System.out.println("General Kenobi!");
+
+/*        // Check if login was successful before loading the main application window
+        if (loginController != null && loginController.loginSuccessful()) {
+            System.out.println("opening main app!");
+            // Use Platform.runLater to run on the JavaFX Application Thread
+            Platform.runLater(() -> openMainApp(primaryStage));
+        }
+
+ */
+
+        assert loginController != null;
+        if (loginController.loginSuccessful) {
+
+            System.out.println("Trying to load MainApplication...");
+
+            openMainApp(primaryStage);
+
+        }
+
+    }
+
+    private LoginController openLoginScreen(Stage primaryStage) {
+
+        System.out.println("Fetching LoginController.fxml");
+
+        String fxmlPath = "LoginController.fxml";
+
+        // Resolve the path to the FXML file
+        Path path = FileSystems.getDefault().getPath(fxmlPath);
+
+        try {
+            // Load FXML file directly from the file system
+            FXMLLoader loader = new FXMLLoader(path.toUri().toURL());
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Login");
+
+            // Pass primaryStage to the controller
+            LoginController loginController = loader.getController();
+            loginController.setPrimaryStage(primaryStage);
+
+            primaryStage.show();
+
+            System.out.println("Fetched and will return in 3 seconds");
+
+            Thread.sleep(3000);
+
+            return loginController;
+
+            //works
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void openMainApp(Stage primaryStage) {
+
+        System.out.println("Fetching GUI.fxml");
+
         String fxmlPath = "GUI.fxml";
 
         // Resolve the path to the FXML file
@@ -30,6 +101,10 @@ public class ClientApp extends Application {
             System.err.println("Error loading FXML file: " + fxmlPath);
             e.printStackTrace();
         }
+    }
+
+    private void printf(String s) {
+        System.out.println(s);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/client/LoginController.java
@@ -0,0 +1,73 @@
+package client;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    boolean loginSuccessful = false;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+
+     private Stage primaryStage;
+
+    // Add this method to set the primaryStage
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    @FXML
+    public void loginButtonClicked() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (isValidCredentials(username, password)) {
+            loginSuccessful = true;
+
+            System.out.println("Login successful.");
+
+/*
+            // Use Platform.runLater to run on the JavaFX Application Thread
+           openMainApp(primaryStage);
+            openMainApp(loginButton.getScene().getWindow());
+*/
+
+        } else {
+            showAlert();
+        }
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        // For demonstration purposes, hardcoding valid credentials
+        return ("admin".equals(username) && "adminkey".equals(password)) ||
+                ("user".equals(username) && "userkey".equals(password));
+    }
+
+
+  /*  private void openMainApp(Window window) {
+        // Close the login window
+        Stage stage = (Stage) window;
+        stage.close();
+        // Open the main app window
+
+    }
+
+
+
+   */
+    private void showAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter valid username and password.", ButtonType.OK);
+        alert.setTitle("Invalid credentials");
+        alert.showAndWait();
+    }
+}
