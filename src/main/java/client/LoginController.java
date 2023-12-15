abde17b580fe6050edf5817010bee7459854d4e0
Jai Singh <134519518+thedizzle69@users.noreply.github.com>
2023-12-15 03:12:02 +0100

    fxml files now in resource directory. Path adapted in all files (#31)
    * deleted duplicate fxml files
    
    * Added login screen but it loops with correct credentials instead of opening the main app. With wrong credentials it correctly prints the error message
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why
    
    * Login screen stuff. Now it gets out of the program after succesful login. Think I know why

--- src/main/java/client/LoginController.java
+++ src/main/java/client/LoginController.java
@@ -36,15 +36,20 @@ public class LoginController {
 
             System.out.println("Login successful.");
 
+        }
+
+
 /*
             // Use Platform.runLater to run on the JavaFX Application Thread
            openMainApp(primaryStage);
             openMainApp(loginButton.getScene().getWindow());
 */
 
+
         } else {
             showAlert();
         }
+
     }
 
     private boolean isValidCredentials(String username, String password) {
