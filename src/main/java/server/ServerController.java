e2cab93ff712d0e1991354fd8fa904eeba1fb8e3
ic22b501 <ic22b501@technikum-wien.at>
2024-01-06 13:29:02 +0100

    export log finished

--- src/main/java/server/ServerController.java
+++ src/main/java/server/ServerController.java
@@ -5,11 +5,19 @@ import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
 
 public class ServerController {
 
@@ -44,6 +52,8 @@ public class ServerController {
         // You need to implement how to get the content from the client
         String content = getContentFromClient();
         screenOutput.displayContent(lvListView.getSelectionModel().getSelectedItem());
+
+        System.out.println(lvListView.getSelectionModel().getSelectedItem());
     }
 
 
@@ -52,7 +62,7 @@ public class ServerController {
     {
         lvListView.setItems(contentList);
 
-    //todo implement list view
+
 
 
 
@@ -67,5 +77,29 @@ public class ServerController {
         // Return the content received from the client
         // This is just a placeholder, you will need to implement the logic to retrieve actual content
         return "Content from client";
+    }
+
+    public void ExportLogButtonPressed(ActionEvent actionEvent) {
+        File ExportLog= new File("LogFile.txt");
+
+        try {
+            FileWriter myWriter= new FileWriter(ExportLog);
+            BufferedWriter bufferedWriter= new BufferedWriter(myWriter);
+            bufferedWriter.write(tfLogField.getText());
+            bufferedWriter.close();
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information");
+        alert.setHeaderText("Export Log sucessful!");
+        alert.setContentText("Path"+ ExportLog.getAbsolutePath().toString());
+        alert.showAndWait();
+
+
+
     }
 }
